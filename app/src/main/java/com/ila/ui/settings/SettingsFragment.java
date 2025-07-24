08b4245ed0e1800c044fd6cb1ca3f9582cfdea10
Andrew Larkin <a.larkin0725@gmail.com>
2025-07-24 18:47:12 -0400

    fixed conflicts

--- app/src/main/java/com/ila/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/ila/ui/settings/SettingsFragment.java
@@ -1,12 +1,17 @@
 package com.ila.ui.settings;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -27,9 +32,43 @@ public class SettingsFragment extends Fragment {
 
         final TextView textView = binding.textSettings;
         SettingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Button AndButton = binding.buttonDarkMode;
+        AndButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ButtonClicked(0);
+            }
+        });
         return root;
     }
 
+    public void ButtonClicked(int i) {
+        switch(i){
+        case 0:
+            Context context = this.getContext();
+            if(isNightMode(context))
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                requireActivity().recreate();
+            }
+            else
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                requireActivity().recreate();
+            }
+            break;
+
+            case 1:
+                break;
+        default:
+            break;
+    }
+
+    }
+    public boolean isNightMode(Context context) {
+        int nightModeFlags = context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        return nightModeFlags == Configuration.UI_MODE_NIGHT_YES;
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
