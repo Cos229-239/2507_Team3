afcd575572db356315ebc3ceaa6b04fed35db3b6
Brianna Gadbois <BAGadbois@student.fullsail.edu>
2025-07-24 17:46:50 -0500

    Updated Homepage backpack, added folder option and buttons. Changed Dashboard class to ClassSelection specifically. Added placeholder buttons for each Class. Adjusted Notification and Settings buttons.

--- app/src/main/java/com/ila/ui/classselection/ClassSelectionFragment.java
+++ app/src/main/java/com/ila/ui/classselection/ClassSelectionFragment.java
@@ -1,4 +1,4 @@
-package com.ila.ui.dashboard;
+package com.ila.ui.classselection;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.ila.databinding.FragmentDashboardBinding;
+import com.ila.databinding.FragmentClassSelectionBinding;
 
-public class DashboardFragment extends Fragment {
+public class ClassSelectionFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentClassSelectionBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        ClassSelectionViewModel classSelectionViewModel =
+                new ViewModelProvider(this).get(ClassSelectionViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentClassSelectionBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textClassSelection;
+        classSelectionViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
