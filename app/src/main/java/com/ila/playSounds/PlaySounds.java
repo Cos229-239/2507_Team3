8099ad2bd84d0e66157d8266c35fc5180bb46a6d
Andrew Larkin <a.larkin0725@gmail.com>
2025-08-09 00:11:44 -0400

    I still managed to mess it up again

--- app/src/main/java/com/ila/playSounds/PlaySounds.java
+++ app/src/main/java/com/ila/playSounds/PlaySounds.java
@@ -6,11 +6,12 @@ import android.media.MediaPlayer;
 public class PlaySounds {
     private static volatile PlaySounds instance;//volatile for fast syncronization
     private final Context context;
+    private MediaPlayer mediaPlayer;
 
     private PlaySounds(Context context) //private constructor
     {
         this.context = context.getApplicationContext();
-        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer = new MediaPlayer();
         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
@@ -33,21 +34,18 @@ public class PlaySounds {
     }
     public void playSound(int soundId)
     {
-        MediaPlayer mediaPlayer;
         mediaPlayer = MediaPlayer.create(context, soundId);
         mediaPlayer.setVolume(10,10);
         mediaPlayer.start();
     }
     public void playSound(Context context,int soundId, int volume)//mono audio volume
     {
-        MediaPlayer mediaPlayer;
         mediaPlayer = MediaPlayer.create(context, soundId);
         mediaPlayer.setVolume(volume,volume);
         mediaPlayer.start();
     }
     public void playSound(Context context,int soundId, int volumeLeft,int volumeRight)//stereo
     {
-        MediaPlayer mediaPlayer;
         mediaPlayer = MediaPlayer.create(context, soundId);
         mediaPlayer.setVolume(volumeLeft,volumeRight);
         mediaPlayer.start();
