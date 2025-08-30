2bae8f49281f2fa1e647c05dedabb389dba583db
Andrew Larkin <a.larkin0725@gmail.com>
2025-08-29 21:49:42 -0400

    Yay Fishys2

--- app/src/main/java/com/ila/minigames/core/mergeFish/GameView.java
+++ app/src/main/java/com/ila/minigames/core/mergeFish/GameView.java
@@ -1,55 +1,76 @@
 package com.ila.minigames.core.mergeFish;
 import java.util.ArrayList;
-
+import android.os.Handler;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.os.Looper;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.content.Context;
+import android.view.ViewTreeObserver;
 
 import com.ila.R;
 
 public class GameView extends View{
-    ArrayList<Fish> fishlist;
-    protected boolean running;
     private Paint paint;
     private Rect spriteBounds;
-    public ArrayList<Fish> getFish(){return fishlist;}
+
     private Bitmap spriteBitmap; // Our image
-    private int spriteX;
-    private int spriteY;
     private final int spriteSize = 100;
 
-        public GameView(Context context, AttributeSet attrs)
-        {
+    private boolean isLooping = false;
+    private Handler handler;
+    private Runnable gameLoop;
+
+    private final long frameRate = 16L; // Approximately 60 frames per second (1000/60)
+
+
+        public GameView(Context context, AttributeSet attrs) {
             super(context, attrs);
+
             paint = new Paint();
             paint.setColor(Color.RED);
-            spriteX = 0;
-            spriteY = 0;
             spriteBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.clown_fish);
-            spriteBounds = new Rect(spriteX, spriteY, spriteX + spriteSize, spriteY + spriteSize);
+            spriteBounds = new Rect(0, 0, spriteSize, spriteSize);
+            handler = new Handler(Looper.getMainLooper());
+            gameLoop = new Runnable() {
+                @Override
+                public void run() {
+                    if (isAttachedToWindow() && isLooping) {
+                        updateGameState();
+                        invalidate();
+                        handler.postDelayed(this, frameRate);
+                    }
+                }
+            };
         }
-
         @Override
-        protected void onDraw(Canvas canvas)
-        {
-            super.onDraw(canvas);
-            canvas.drawBitmap(spriteBitmap, null, spriteBounds, paint);
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // Draw the bitmap onto the canvas within the specified bounds
+        canvas.drawBitmap(spriteBitmap, null, spriteBounds, paint);
+    }
+    public void startGameLoop() {
+        if (!isLooping) {
+            isLooping = true;
+            handler.post(gameLoop);
         }
-        public void stopGame()
-        {
-        running = false;
+    }
+
+    public void stopGameLoop() {
+        if (isLooping) {
+            isLooping = false;
+            handler.removeCallbacks(gameLoop);
         }
-        public void nxtFrame(){
-        int size = fishlist.size();
-        for(int i = 0; i < size; i++){
-            fishlist.get(i).swim();
-        }
-        invalidate();
-        }
+    }
+    public void updateGameState()
+    {
+        return;
+    }
+
 }
